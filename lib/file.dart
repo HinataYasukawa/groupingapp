0c1463742466b40d23491d3c1a7f76c45f7f0741
ei1906 <whitegorilla1906@gmail.com>
2024-01-15 16:52:33 +0900

    色々な関数の外形を作成

--- lib/file.dart
+++ lib/file.dart
@@ -22,6 +22,9 @@ List<List<String>> read_file(String path) {
   }
 }
 
+// 文字列データである特徴量群dataについて、整数型の特徴量群に整形する関数
+List<List<int>> format_data(List<List<String>> data) {}
+
 // 行と列を入れ替える関数
 List<List<String>> replace_row_column(List<List<String>> data) {
   int col_len = data.length;
@@ -68,6 +71,19 @@ List<int> do_random_grouping(int target, int group) {
   return ret;
 }
 
+// 特徴量dataを基にクラスター分析を行い、そこからグループごとの能力が均等になるように割り振る関数
+List<int> do_balance_grouping(List<List<int>> data, int group) {
+  // クラスター分析の結果を取得
+  List<int> cls = do_cluster_analysis(data, group);
+}
+
+// 特徴量dataを基にクラスター分析を行い、そこからメンバーの能力が近くなるように割り振る関数
+List<int> do_nearing_grouping(List<List<int>> data, int group) {
+  // クラスター分析の結果を取得
+  List<int> cls = do_cluster_analysis(data, group);
+}
+
+// 特徴量dataを基にクラスター数groupでクラスター分析を行う関数
 List<int> do_cluster_analysis(List<List<int>> data, int group) {
   // ここでpythonを呼び出してクラスター分析を実施
   // List<int> res = execute(ClusterAnalysis.py); <- ここ嘘なので直して
